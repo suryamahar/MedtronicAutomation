9f684d8f3a8da63b01a2b90a6416f2b0f05a311d
SYNAPSE\surya.mahar <kj>
2024-08-13 12:27:51 +0530

    Changes in files

--- medtronic/Login/login_motherobject.py
+++ medtronic/Login/login_motherobject.py
@@ -1,16 +1,14 @@
 class LoginMotherObject:
     @staticmethod
     def valid_credentials():
-        return{
-            'username':'developer_1',
-            'password':'Developer_1'
+        return {
+            'username': 'developer_1',
+            'password': 'Developer_1'
         }
 
     @staticmethod
     def invalid_credentials():
-        return{
-            'username':'eveloper_1',
-            'password':'Developer_1'
+        return {
+            'username': 'eveloper_1',
+            'password': 'Developer_1'
         }
-
-
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,26 +1,22 @@
-from playwright.async_api import Page,expect
+from playwright.async_api import Page, expect
 from src.pages.LoginPage import LoginPage
 from src.pages.HomePage import HomePage
 from medtronic.Login.login_motherobject import LoginMotherObject
 from config.config import TestData
 
+
 def test_valid_login(login_to_admin):
-    page=login_to_admin
-    loginPage=LoginPage(page)
-    loginPage.do_login(LoginMotherObject.valid_credentials())
-    homepage=HomePage(page)
+    page = login_to_admin
+    login_page = LoginPage(page)
+    login_page.do_login(LoginMotherObject.valid_credentials())
+    homepage = HomePage(page)
     homepage.verify_title(TestData.LOGIN_PAGE_TITLE)
     homepage.verify_valid_login()
 
+
 def test_invalid_login(login_to_admin):
-    page=login_to_admin
-    loginPage=LoginPage(page)
-    loginPage.do_login(LoginMotherObject.invalid_credentials())
-    homepage=HomePage(page)
+    page = login_to_admin
+    login_page = LoginPage(page)
+    login_page.do_login(LoginMotherObject.invalid_credentials())
+    homepage = HomePage(page)
     homepage.verify_invalid_login()
-
-
-
-
-
-
