a88acb5c030749683db0589f1195913f9ff60cfd
SYNAPSE\surya.mahar <kj>
2024-08-16 11:30:57 +0530

    changes

--- medtronic/Homepage/homepage_motherobject.py
+++ medtronic/Homepage/homepage_motherobject.py
@@ -1,9 +1,13 @@
+from src.utils.action_helper import ActionHelper
+
+
 class HomepageMotherObject:
 
     @staticmethod
     def automation_project():
+        random_number = str(ActionHelper.generate_random_number())
         return {
-            'project_title': 'Automated_Project',
+            'project_title': 'Automated_Project' + random_number,
             'description': 'Automated_Project_desc',
             'operating_unit_id': 'Cardiovascular_CAS',
             'cost_center': 'cost',
@@ -12,10 +16,10 @@ class HomepageMotherObject:
 
     @staticmethod
     def automation_workbench():
+        random_number = str(ActionHelper.generate_random_number())
         return {
-            'workbench_name': 'Automated_workbench',
-            'workbench_description': 'Automated_Project_desc',
+            'workbench_name': 'Automated_workbench'+random_number,
+            'workbench_description': 'Automated_Project_desc'+random_number,
             'workbench_group': 'Non-GPU',
             'instance_type': 't2.micro tester',
         }
-
